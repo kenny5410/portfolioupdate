4b70ad0bb55a36f8bd975ee77ae6f0d61f69e505
Kenny <nels5410@gmail.com>
2019-02-09 16:42:16 -0600

    basic html added

---
+++ javascript/logic.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAyhC2YSXWS77Lhcvi6e-_PJ1_fU12aghM",
+    authDomain: "kenneth-sportfolio.firebaseapp.com",
+    databaseURL: "https://kenneth-sportfolio.firebaseio.com",
+    projectId: "kenneth-sportfolio",
+    storageBucket: "kenneth-sportfolio.appspot.com",
+    messagingSenderId: "84427282848"
+};
+firebase.initializeApp(config);
